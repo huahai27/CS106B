7fac92e9deb6886630e4baedc0e7f8e109b726ce
Yongheng Liu <161477977+huahai27@users.noreply.github.com>
2025-03-13 09:36:45 +0800

    Did section2

---
+++ section/section2/OrgChart.cpp
@@ -0,0 +1,61 @@
+/*
+ * CS106B Section Handout Test Harness: Section 2
+ * ----------------------------------------------
+ * These problems have been galvanized from years of
+ * section handouts that have been worked on by numerous
+ * instructors and TA's. Codified for Qt and Ed by Neel
+ * Kishnani for Winter 2022.
+ *
+ * A huge thank you to Keith Schwarz and Julie Zelenski
+ * for creating an amazing testing harness!
+ */
+
+#include "testing/SimpleTest.h"
+#include "map.h"
+#include <string>
+using namespace std;
+
+string findCEO(const string& p,
+               const Map<string, string>& bosses) {
+    if (!bosses.containsKey(p)) {
+        return p;
+    } else {
+        return findCEO(bosses.get(p), bosses);
+    }
+}
+
+bool areAtSameCompany(const string& p1,
+                      const string& p2,
+                      const Map<string, string>& bosses) {
+    string boss1 = findCEO(p1, bosses);
+    string boss2 = findCEO(p2, bosses);
+
+    return boss1 == boss2;
+}
+
+/* * * * * Provided Tests Below This Point * * * * */
+
+PROVIDED_TEST("Basic tests for areAtSameCompany") {
+    Map<string, string> company1 = {
+        { "Neel", "Keith" },
+        { "Keith", "Mehran" },
+        { "Mehran", "John" }, 
+        { "John", "Jennifer" }, 
+        { "Jennifer", "Persis" }
+    };
+
+    Map<string, string> company2 = {
+        { "CJ", "Pierre" },
+        { "Jack", "Stanley" },
+        { "Stanley", "Lisa" },
+        { "Lisa", "Janet" },
+        { "Janet", "Poonam" }
+
+    };
+
+    EXPECT(areAtSameCompany("Neel", "Jennifer", company1));
+    EXPECT(!areAtSameCompany("Neel", "Lisa", company1));
+    EXPECT(!areAtSameCompany("CJ", "Keith", company2));
+    EXPECT(!areAtSameCompany("Poonam", "Lisa", company1));
+
+}
